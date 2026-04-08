093c4b697451c572431418cf53bb2c4f74c48e3b
Piyapat.J <Piyapat.jun@spumail.net>
2026-04-08 14:15:58 +0700

    fix: AppRegistryNotReady in apps.py

--- core/apps.py
+++ core/apps.py
@@ -1,20 +1,11 @@
 from django.apps import AppConfig
 
-from django.contrib.auth import get_user_model
-
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
 
     def ready(self):
+        from django.contrib.auth import get_user_model
+        User = get_user_model()
+        
         import core.signals
-        create_superuser()     
-User = get_user_model()
-
-def create_superuser():
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@gmail.com",
-            password="12345678"
-        )
